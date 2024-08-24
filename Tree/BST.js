1b324ef99b3a4caf8b2ac07ef66cd2d89de4605d
Manas0410 <manasshrivastava0410@gmail.com>
2024-08-24 20:57:02 +0530

    bst del

--- Tree/BST.js
+++ Tree/BST.js
@@ -7,7 +7,46 @@ class BST {
 
   //   insert a node
 
-  insertNode(v) {
-    if(this.)
+  insertNode(head, val) {
+    let node = new BST(val);
+    if (!head) return node;
+
+    if (val < head.value) head.left = this.insertNode(head.left, val);
+    else head.right = this.insertNode(head.right, val);
+
+    return head;
   }
+
+  //   deletion of node
 }
+
+const bst = new BST(3);
+
+bst.insertNode(bst, 1);
+bst.insertNode(bst, 2);
+bst.insertNode(bst, 3);
+bst.insertNode(bst, 4);
+bst.insertNode(bst, 5);
+bst.insertNode(bst, 6);
+
+console.log(bst);
+
+// deletion of a node
+var deleteNode = function (root, key) {
+  if (!root) return null;
+
+  if (key < root.val) root.left = deleteNode(root.left, key);
+  else if (key > root.val) root.right = deleteNode(root.right, key);
+  else {
+    if (!root.right) return root.left;
+    else if (!root.left) return root.right;
+
+    let cur = root.right;
+    while (cur.left) {
+      cur = cur.left;
+    }
+    root.val = cur.val;
+    root.right = deleteNode(root.right, root.val);
+  }
+  return root;
+};
